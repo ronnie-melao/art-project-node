8e57d017a83ce03e21ad6eea5d6a90b63db64c59
Ronnie Melao <ronniemelao@yahoo.com>
2024-05-06 17:39:34 -0400

    populated incoming commission requests

--- public/js/commissions_script.js
+++ public/js/commissions_script.js
@@ -4,13 +4,20 @@ function populateCommissionsTable() {
   tableBody.innerHTML = "";
 
   // Iterate over commissionsArray and create table rows
-  commissionsArray.forEach(request => {
+  commissionsArray.forEach((request, index) => {
     const row = document.createElement("tr");
     row.innerHTML = `
         <td>${request.requesterUsername}</td>
         <td>${request.description}</td>
-        <td>${request.price}</td>
-        <td>${request.status}</td>
+        <td>$${request.price}</td>
+        <td>
+        <select id="status_${index}" commission-id="${request._id}">
+        <option value="Pending" ${request.status === "Pending" ? "selected" : ""}>Pending</option>
+        <option value="Accepted" ${request.status === "Accepted" ? "selected" : ""}>Accepted</option>
+        <option value="Denied" ${request.status === "Denied" ? "selected" : ""}>Denied</option>
+        <option value="Completed" ${request.status === "Completed" ? "selected" : ""}>Completed</option>
+        </select>
+      </td>
       `;
     tableBody.appendChild(row);
   });
@@ -18,3 +25,30 @@ function populateCommissionsTable() {
 
 if (commissionsArray.length > 0)
   populateCommissionsTable();
+
+
+document.querySelectorAll('select').forEach(select => {
+  select.addEventListener('change', async event => {
+    const commissionId = event.target.getAttribute('commission-id')
+    const newStatus = event.target.value;
+    
+    try {
+      const response = await fetch('/commissions/change-status', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ commissionId, newStatus })
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update status');
+      }
+
+      // Handle success response
+      console.log('Status updated successfully');
+    } catch (error) {
+      console.error('Error updating status:', error.message);
+    }
+  });
+});
