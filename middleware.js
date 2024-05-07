d50a7bbdd6a113232fc808ef7a77f3d0f2567dd0
andrew <cowslayer6789@gmail.com>
2024-05-06 21:08:39 -0400

    forbid a bunch of routes from non-users/artists

--- middleware.js
+++ middleware.js
@@ -38,8 +38,8 @@ const nonUsersOnlyMDWare = async (req, res, next) => {
     next();
   }
 };
-router.route("/login").get(nonUsersOnlyMDWare);
-router.route("/register").get(nonUsersOnlyMDWare);
+router.route("/login").all(nonUsersOnlyMDWare);
+router.route("/register").all(nonUsersOnlyMDWare);
 const userOnlyMDWare = async (req, res, next) => {
   if (req.session?.user) {
     next();
@@ -50,6 +50,11 @@ const userOnlyMDWare = async (req, res, next) => {
 
 router.route("/logout").get(userOnlyMDWare);
 router.route("/profile").get(userOnlyMDWare);
+router.route("/switchProfile").all(userOnlyMDWare);
+router.route("/liked").all(userOnlyMDWare);
+router.route("/settings").all(userOnlyMDWare);
+router.route("/commissions").all(userOnlyMDWare);
+router.route("/commission_request").all(userOnlyMDWare);
 router.route("/review/:id").all(userOnlyMDWare);
 
 const artistOnlyMDWare = async (req, res, next) => {
@@ -65,5 +70,6 @@ const artistOnlyMDWare = async (req, res, next) => {
 router.route("/posts/create").all(artistOnlyMDWare);
 router.route("/posts/edit").all(artistOnlyMDWare);
 router.route("/posts/delete").all(artistOnlyMDWare);
+router.route("/commissions/change-status").all(artistOnlyMDWare);
 
 export default router;
